1c9362377114ca287efa8a8a9f204a2332b91530
RafaelRCLima <rafael.lima19-92@hotmail.com>
2020-01-17 17:23:47 -0300

    Insercao de Cliente com Post

--- src/main/java/com/rafaelrlima/cursomc/resources/ClienteResource.java
+++ src/main/java/com/rafaelrlima/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.rafaelrlima.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafaelrlima.cursomc.domain.Cliente;
 import com.rafaelrlima.cursomc.dto.ClienteDTO;
+import com.rafaelrlima.cursomc.dto.ClienteNewDTO;
 import com.rafaelrlima.cursomc.services.ClienteService;
 
 @RestController
@@ -30,6 +33,15 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
@@ -63,4 +75,5 @@ public class ClienteResource {
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
+	
 }
